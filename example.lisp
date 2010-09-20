370548436fdc7f6f30c00686a6f59a53275b595d
Yoshinori Tahara <read.eval.print@gmail.com>
2010-09-20 10:31:13 +0900

    README を追加。

--- example.lisp
+++ example.lisp
@@ -23,3 +23,17 @@
              do (format t "~&~d" i)
            do (setf (value db k) v)
            do (assert (equal v (value db k)))))))
+
+
+(defun example2 ()
+  (time
+   (with-db (db "/tmp/tottori-nando-example")
+     (loop for i from 1 to 10000
+           for k = (format nil "key~a" i)
+           for v = (format nil "value~a" i)
+           do (setf (value db k) v))
+     (loop for i from 1 to 10000
+           for k = (format nil "key~a" i)
+           for v = (format nil "new-value~a" i)
+           do (setf (value db k) v)
+           do (assert (equal v (value db k)))))))
